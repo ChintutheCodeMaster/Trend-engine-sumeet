28657cb40a28556febb1e83a51b7c3931e5f1dc2
SumeetB <150938727+ChintutheCodeMaster@users.noreply.github.com>
2026-03-23 04:58:45 +0530

    changes

--- trend-pipeline/.next/static/chunks/webpack.js
+++ trend-pipeline/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "634d7221fac95d80"; }
+/******/ 		__webpack_require__.h = function() { return "bfdf2d80c55cfb47"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
